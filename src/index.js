8ef87198aeaa0fa617154700cc60d78379027826
drozhin <drozhin@qnium.com>
2017-05-11 17:06:13 +0300

    Removed DialogService.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import DataProviderRegistry from './services/DataProviderRegistry';
 import DataProviderJSONFile from './providers/DataProviderJSONFile';
 import DataProviderJSONService from './providers/DataProviderJSONService';
-import DialogService, {DialogResult} from './services/DialogService';
 
 import InputFilterController from './controllers/InputFilterController';
 import { ListController, ListControllerEvents } from './controllers/ListController';
 import SelectFilterController from './controllers/SelectFilterController';
 import ValidationController from './controllers/ValidationController';
 
-export { DataProviderRegistry, DataProviderJSONFile, DataProviderJSONService, DialogService, DialogResult, ListController,
+export { DataProviderRegistry, DataProviderJSONFile, DataProviderJSONService, ListController,
     ListControllerEvents, InputFilterController, SelectFilterController, ValidationController
  }
